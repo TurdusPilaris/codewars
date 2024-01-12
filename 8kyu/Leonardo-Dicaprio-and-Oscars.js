e02e47a3c5a49f5632e9e5fbe2f0f62e679326ca
drozdovalena <drozdova.el.gen@gmail.com>
2024-01-12 22:39:05 +0100

    kata Leo and Oscar other solution

--- 8kyu/Leonardo-Dicaprio-and-Oscars.js
+++ 8kyu/Leonardo-Dicaprio-and-Oscars.js
@@ -5,13 +5,17 @@
 
 function leo(oscar){
 
-    if(oscar===88){
-        return("Leo finally won the oscar! Leo is happy");
-    } else if (oscar===86) {
-        return("Not even for Wolf of wallstreet?!");
-    } else if (oscar < 88) {
-        return ("When will you give Leo an Oscar?");
-    } else {
-        return ("Leo got one already!");
-    }
+    return (oscar === 88) ? "Leo finally won the oscar! Leo is happy" :
+        (oscar === 86) ? "Not even for Wolf of wallstreet?!" :
+        (oscar < 88) ? "When will you give Leo an Oscar?" : "Leo got one already!";
+
+    // if(oscar===88){
+    //     return( "Leo finally won the oscar! Leo is happy");
+    // } else if (oscar===86) {
+    //     return("Not even for Wolf of wallstreet?!");
+    // } else if (oscar < 88) {
+    //     return ("When will you give Leo an Oscar?");
+    // } else {
+    //     return ("Leo got one already!");
+    // }
 }
