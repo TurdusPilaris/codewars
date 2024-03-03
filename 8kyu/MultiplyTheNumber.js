ae135e0f5e6c44717f05094fd688d0e07ff44395
drozdovalena <drozdova.el.gen@gmail.com>
2024-03-03 20:00:48 +0100

    kata multyply the number  my solution

---
+++ 8kyu/MultiplyTheNumber.js
@@ -0,0 +1,9 @@
+function multiply(number){
+    return number*(Math.pow(5, Math.abs(number).toString().length))
+}
+
+console.log(multiply(10))//,250);
+console.log(multiply(5))//,25);
+console.log(multiply(200))//,25000);
+console.log(multiply(0))//,0);
+console.log(multiply(-2))//,-10);
