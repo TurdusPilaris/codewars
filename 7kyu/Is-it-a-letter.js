62a546b07d2b2ae64d0cc9153fb1683f2dc0f2c1
drozdovalena <drozdova.el.gen@gmail.com>
2024-03-29 13:12:52 +0100

    kata "IS IT A LETTER" best practic

--- 7kyu/Is-it-a-letter.js
+++ 7kyu/Is-it-a-letter.js
@@ -1,8 +1,12 @@
 // Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
 
 function isItLetter(character) {
-    const allLetters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-    return (allLetters.includes(character.toLocaleUpperCase()))
+    // const allLetters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    // return (allLetters.includes(character.toLocaleUpperCase()))
+
+    // if( character.toLowerCase() === character.toUpperCase()) return false;
+    // return true
+    return /[a-z]/i.test(character);
 }
 
 console.log( isItLetter('a'))//, true, `'a' is a letter` );
